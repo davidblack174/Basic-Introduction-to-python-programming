99196736df3c782ef91e88908fb2b123ef910609
David Odediran <davidblack174@gmail.com>
2024-07-26 17:46:14 +0000

    creating Arithmetic Operator file

---
+++ Arithmetic Operator.py
@@ -0,0 +1,34 @@
+#----------------------------------------
+# Arithmetic Operator
+#----------------------------------------
+#They are used to perform mathematical operations like addition, subtraction, multiplication, division, etc.
+# Addition
+a = 10
+b = 20
+c = a + b
+print(c)    #This will print 30
+#-------------------------------------------------------
+# Subtraction
+d = a - b
+print(d)    #This will print -10
+#-------------------------------------------------------
+# Multiplication
+e = a * b   
+print(e)    #This will print 200
+#-------------------------------------------------------
+# Division
+f = b / a
+print(f)    #This will print 2.0
+#-------------------------------------------------------
+# Modulus
+g = b % a
+print(g)    #This will print 0
+#-------------------------------------------------------
+# Exponentiation
+h = a ** b
+print(h)    #This will print 100000000000000000000
+#-------------------------------------------------------
+# Division without the remainder
+i = b // a
+print(i)    #This will print 2
+#-------------------------------------------------------
